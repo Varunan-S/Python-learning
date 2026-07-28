cdccf2724ec3f2f65e12ae57e6d173e4923866de
Varunan S <Varunan.sendhil@gmail.com>
2026-07-28 13:27:53 -0400

    Join

---
+++ Day9.py
@@ -0,0 +1,23 @@
+# String strip
+text = "   python   "
+print (text.strip())
+
+# lsstrip
+t = "   python"
+print  (text.lstrip())
+
+# rstrip
+txt = "python   "
+print (txt.rstrip())
+
+# Replace
+S = "I like Wanda"
+print (S.replace("Wanda", "Spiderman"))
+
+# Split 
+k = "I love you"
+print (k.split())
+
+# Join
+alpha = ["B", "A", "T"]
+print ("-".join(alpha))
