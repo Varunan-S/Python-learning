e64e5c9319f076f3932dd0bb7a0c4caf0842f148
Varunan S <Varunan.sendhil@gmail.com>
2026-07-29 14:30:11 -0400

    string concepts completed

--- Day10.py
+++ Day10.py
@@ -5,3 +5,23 @@ print (D.endswith("gy"))
 # isdigit
 print ("2478".isdigit())
 print ("1237ab".isdigit())
+
+# isalpha
+print ("adfg".isalpha())
+print ("6fgd".isalpha())
+
+# isalmun
+print ("hag43".isalnum())
+print ("sg 47".isalnum())
+
+# islower
+print ("x".islower())
+
+# isupper
+print ("Z".isupper())
+
+# isspace
+print ("  ".isspace())
+
+# escapecharacters
+print ("i\n am\n Varunan")
