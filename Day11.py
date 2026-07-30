a315b446dea32431c94ff5edbe66c35f4416238e
Varunan S <Varunan.sendhil@gmail.com>
2026-07-30 13:24:01 -0400

    Palindrome Check

--- Day11.py
+++ Day11.py
@@ -3,8 +3,16 @@ word = "Varun"
 print (word[::-1])
 
 # Reverse without bulit in function
-num = input ("enter the string: ")
+num = "food"
 reverse = ""
 for i in num:
     reverse = i + reverse
 print ("reversed string is", reverse)
+
+# Palindrome check
+w = input ("enter the string: ")
+if w == w[::-1]:
+    print ("String is a palindrome")
+
+else:
+    print ("string is false")
