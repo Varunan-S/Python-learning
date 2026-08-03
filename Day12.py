4369027d555b2f41cdc2a377821ce3978c238a5f
Varunan S <Varunan.sendhil@gmail.com>
2026-08-03 13:19:43 -0400

    append, list indexing and updating list

--- Day12.py
+++ Day12.py
@@ -5,3 +5,19 @@ print (car)
 # list with different data types
 house = [3,"house",6.8,False] 
 print (house)
+
+# list indexing
+box = ["ball","toy","blocks"]
+print (box[0])
+print (box[1])
+print (box[2])
+
+# updating list elements
+chest = ["gold","silver","diamond"]
+chest [0]="key"
+print (chest)
+
+# append
+num = [3, 9, 0, 1]
+num.append(3)
+print (num)
