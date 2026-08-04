e6872d917b9d3ecf7dc8914ffd0c35639bef0bfb
Varunan S <Varunan.sendhil@gmail.com>
2026-08-04 13:45:42 -0400

    Count, Index, Sort

---
+++ Day13.py
@@ -0,0 +1,15 @@
+# findidng elements
+# Index
+cows = ["maadu", "aadu", "paadu"]
+print (cows.index("aadu"))
+
+# Count
+n = [2,4,5,6,2,3,7,2]
+print (n.count(2))
+
+# Sorting
+a = [4,2,5,1,3]
+a.sort()
+print (a)
+a.sort(reverse=True)
+print (a)
