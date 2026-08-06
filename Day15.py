78c5e730cadd1bde0f621e203c7a4f43035dbfd6
Varunan S <Varunan.sendhil@gmail.com>
2026-08-06 13:52:30 -0400

    Tupple

---
+++ Day15.py
@@ -0,0 +1,11 @@
+# New concept-Tuple
+num = "loki"
+print (num)
+
+# Empty tupple
+T = (5,)
+print (type(T))
+
+# Multiple elements
+fruits = ("orange", "apple", "watermellon", "peach")
+print (fruits)
