e30894f3cf0d8f2e12694b5edf2e30d23924e719
Varunan S <Varunan.sendhil@gmail.com>
2026-09-01 12:44:04 -0400

    Func with loops

--- Day21.py
+++ Day21.py
@@ -1,13 +1,19 @@
 # Func with conditions
-def house(n):
-    if n > 0:
-        return "positive"
-    elif n < 0:
-        return "negative"
-    else:
-        return "zero"
+#def house(n):
+    #if n > 0:
+        #return "positive"
+    #elif n < 0:
+#        return "negative"
+    #else:
+        #return "zero"
 
-print(house(2))
-print(house(-4))
-print(house(0))
+#print(house(2))
+#print(house(-4))
+#print(house(0))
 
+# Function with loops
+def ball(r):
+    for i in range(1, r+1):
+        print(i)
+
+ball(5)
