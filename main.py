83a427f128a4763847964d54f8e91efb048bd422
Varunan S <Varunan.sendhil@gmail.com>
2026-09-12 14:22:46 -0400

    Alias

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import calculator
 print(calculator.add(2, 5))
 print(calculator.sub(2, 5))
+
+# specific func in mod
+from calculator import add
+print(add(7, 5))
+
+# Alias in mod
+import calculator as cal
+print(cal.sub(2, 2))
