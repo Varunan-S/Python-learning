a66de13ec6df82b9c6b4ae1e794dd40936008bf3
Varunan S <Varunan.sendhil@gmail.com>
2026-09-13 13:47:25 -0400

    multiple exception handling

--- Day23.py
+++ Day23.py
@@ -1,13 +1,23 @@
 # Exception handling-Handling an error knwoing it will occur prior to it happening, so the program can run smoothly.
 # keywords for Exception handling-try, except
+##try:
+   # a = 10
+  #  b = 0
+ #   print(a/b)
+#except:
+#    print("error expected")
+#try:
+ #   age = int(input("enter the age"))
+#    print(age)
+#except:
+#    print("Please enter a valid number")
+
+# Multiple exception handling
 try:
-    a = 10
-    b = 0
+    a = int(input("Number required"))
+    b = int(input("Number required"))
     print(a/b)
-except:
-    print("error expected")
-try:
-    age = int(input("enter the age"))
-    print(age)
-except:
-    print("Please enter a valid number")
+except ValueError:
+    print("Numbers only please")
+except ZeroDivisionError:
+    print("Cannot be divided by zero")
